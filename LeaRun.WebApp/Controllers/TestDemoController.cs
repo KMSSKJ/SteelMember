4374f93678ec61c8cee9f1e79fefc62ef301cefe
汤富豪 <741051180@qq.com>
2017-05-15 16:55:51 +0800

    0012

--- LeaRun.WebApp/Controllers/TestDemoController.cs
+++ LeaRun.WebApp/Controllers/TestDemoController.cs
@@ -64,13 +64,78 @@ namespace LeaRun.WebApp.Controllers
                 Directory.CreateDirectory(path);
                 if (!System.IO.File.Exists(fullFileName))
                 {
-                    Filedata.SaveAs(fullFileName);                    
+                    Filedata.SaveAs(fullFileName);
                 }
                 return Content(virtualPath);
             }
             catch (Exception ex)
             {
                 return Content(ex.Message);
+            }
+        }
+
+        //泛型接口 
+        interface IFanXingable<T>
+        {
+            T SayNumber(T t);
+        }
+        //泛型类 
+        public class FanXing<T>// where T:class,new() 
+        {
+            T[] arryStrings = new T[]
+            {
+
+            };
+            //索引器 
+            public T this[int index]
+            {
+                get
+                {
+                    return arryStrings[index];
+                }
+                set
+                {
+                    arryStrings[index] = value;
+                }
+            }
+            //泛型方法 
+            public T Add(T str)
+            {
+                return str;
+            }
+        }
+
+        public class Person
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+
+
+            public void Add<T>(T t)
+            {
+                Console.WriteLine(t);
+            }
+        }
+        static void Main(string[] args)
+        { //实例化 
+            FanXing<string> fanXing = new FanXing<string>();
+            fanXing[0] = "aaa1"; fanXing[1] = "aaa2";
+            fanXing[2] = "aaa3"; fanXing[3] = "aaa4";
+            FanXing<int> fanXingInt = new FanXing<int>();
+            fanXingInt[0] = 1; fanXingInt[2] = 2;
+            fanXingInt[3] = 4; fanXingInt[4] = 3;
+            FanXing<Person> fanXingClass = new FanXing<Person>();
+            fanXingClass[0] = new Person() { Name = "guo", Age = 21 };
+            fanXingClass[0] = new Person() { Name = "yang", Age = 20 };
+            //方法泛型 泛型是数据类型的抽象 数据类型是对象的抽象 
+            Console.ReadKey();
+        }
+        //实现接口 
+        public class MyClass : IFanXingable<Int32>
+        {
+            public int SayNumber(int t)
+            {
+                return t;
             }
         }
     }
